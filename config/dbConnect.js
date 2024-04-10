17333b7584329f4fb956d747028a1f841a711bb2
crdd1site <166614204+crdd1site@users.noreply.github.com>
2024-04-10 11:52:44 -0300

    Update dbConnect.js

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 mongoose.set('strictQuery', true)
 
-mongoose.connect("mongodb+srv://crddsite:OGcXlBOjjDsUO2DI@cluster0.1giikco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://crddsite:OGcXlBOjjDsUO2DI@cluster0.1giikco.mongodb.net/")
 
 let db = mongoose.connection;
 
